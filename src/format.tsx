e9f5fe03a5c30b4e8ee976f8c71585e162fe513e
Joe <aidevjoe@gmail.com>
2023-06-19 09:40:00 +0800

    Update format.tsx

--- src/format.tsx
+++ src/format.tsx
@@ -12,10 +12,3 @@ export default function Format(props: LaunchProps<{ arguments: SQLFormatArgument
     const markdown = `\`\`\`sql\n${content}\n\`\`\``
     return <Detail markdown={markdown}></Detail>
 }
-
-
-// SELECT COUNT(*) as count, sum(price) as total_money, (select name from `products` where id = `product_id`) as name, (select platform_type from `products` where id = `product_id`) as platform_type
-// FROM orders
-// WHERE
-// date_format(`purchase_date`,'%Y-%m')=date_format(date_sub(curdate(), interval 1 month),'%Y-%m') AND deleted_at IS NULL
-// GROUP BY product_id  ORDER BY count desc
