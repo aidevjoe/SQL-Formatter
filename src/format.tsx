cce55eb5f6c467551e3881bd0cceef4bfd0aee71
Joe <aidevjoe@gmail.coma>
2023-06-27 11:21:02 +0800

    Add "Copy minified SQL to clipboard" Action

--- src/format.tsx
+++ src/format.tsx
@@ -7,15 +7,19 @@ interface SQLFormatArguments {
 
 export default function Format(props: LaunchProps<{ arguments: SQLFormatArguments }>) {
     const { code } = props.arguments;
-    const content = format(code, { language: 'mysql' })
+    const content = format(code, { language: 'mysql', keywordCase: 'upper' });
 
     const markdown = `\`\`\`sql\n${content}\n\`\`\``
     return <Detail
     actions={
       <ActionPanel>
         <Action.CopyToClipboard
-          title="Copy Result"
+          title="Copy To Clipboard"
           content={content}
+        />
+        <Action.CopyToClipboard
+          title="Copy Minified SQL To Clipboard"
+          content={content.replace(/\s+/g, ' ')}
         />
       </ActionPanel>
     } markdown={markdown}></Detail>
